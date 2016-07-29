76d8e7b6c3b74462efe63516385b4eb3948225dd
Donovan Solms <donovan.solms@gmail.com>
2016-07-29 15:15:11 +0200

    Added initial tests
    Added Travis CI config

---
+++ ioRPC.Test/ioCommandTest.cs
@@ -0,0 +1,46 @@
+﻿using Limitless.ioRPC.Structs;
+using NUnit.Framework;
+
+namespace ioRPC.Test
+{
+    [TestFixture]
+    public class ioCommandTest
+    {
+        [Test]
+        public void ShouldSetCommandName()
+        {
+            string commandNameTest = "testCommand";
+            ioCommand command = new ioCommand(commandNameTest);
+            string commandName = command.Name;
+            Assert.That(commandName, Is.SameAs(commandNameTest));
+        }
+
+        [Test]
+        public void ShouldInitializeParametersList()
+        {
+            ioCommand command = new ioCommand();
+            command.Parameters.Add("testParam");
+            Assert.That(command.Parameters.Count, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void ShouldNotParse()
+        {
+            ioCommand command;
+            bool parseResult = ioCommand.TryParse("this is invalid xml", out command);
+            Assert.That(parseResult, Is.False);
+        }
+
+        [Test]
+        public void ShouldParse()
+        {
+            string commandXml = "<?xml version=\"1.0\" encoding=\"utf-16\"?><ioCommand xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"><Name>testCommand</Name><Parameters><anyType xsi:type=\"xsd:string\">testParam</anyType></Parameters></ioCommand>";
+
+            ioCommand command;
+            bool parseResult = ioCommand.TryParse(commandXml, out command);
+            Assert.That(parseResult, Is.True);
+            Assert.That(command.Name, Does.Contain("testCommand"));
+            Assert.That(command.Parameters.Count, Is.EqualTo(1));
+        }
+    }
+}
---
+++ ioRPC.Test/ioResultTest.cs
@@ -0,0 +1,47 @@
+﻿using Limitless.ioRPC.Structs;
+using NUnit.Framework;
+
+namespace ioRPC.Test
+{
+    [TestFixture]
+    public class ioResultTest
+    {
+        [Test]
+        public void ShouldSetCommandName()
+        {
+            string resultNameTest = "testCommand";
+            ioResult result = new ioResult(resultNameTest);
+            string resultName = result.CommandName;
+            Assert.That(resultName, Is.SameAs(resultNameTest));
+        }
+
+        [Test]
+        public void ShouldSetData()
+        {
+            string dataTest = "testData";
+            ioResult result = new ioResult("testCommand", dataTest);
+            object resultData = result.Data;
+            Assert.That(resultData, Is.SameAs(dataTest));
+        }
+
+        [Test]
+        public void ShouldNotParse()
+        {
+            ioResult result;
+            bool parseResult = ioResult.TryParse("this is invalid xml", out result);
+            Assert.That(parseResult, Is.False);
+        }
+
+        [Test]
+        public void ShouldParse()
+        {
+            string resultXml = "<?xml version=\"1.0\" encoding=\"utf-16\"?><ioResult xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"><CommandName>testCommand</CommandName><ExceptionMessage /><Data xsi:type=\"xsd:string\">testData</Data></ioResult>";
+
+            ioResult result;
+            bool parseResult = ioResult.TryParse(resultXml, out result);
+            Assert.That(parseResult, Is.True);
+            Assert.That(result.CommandName, Does.Contain("testCommand"));
+            Assert.That(result.Data, Does.Contain("testData"));
+        }
+    }
+}
