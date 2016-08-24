18bcdfa936fdd17d117050e73e36992da3a04d0e
Donovan Solms <donovan.solms@gmail.com>
2016-08-24 22:17:16 +0200

    Added tests coverage batch file to after_test

--- SubmitCoverage.bat
+++ SubmitCoverage.bat
@@ -1,7 +1,5 @@
 @echo on
 echo NuGet installing coveralls.net package
-dir
-dir "TestResults"
 nuget install coveralls.net -Version 0.7.0 -OutputDirectory tools
 echo Submitting coverage report to coveralls.io
 .\tools\coveralls.net.0.7.0\tools\csmacnz.Coveralls.exe --opencover -i .\TestResults\Coverage.xml --commitAuthor "%APPVEYOR_REPO_COMMIT_AUTHOR%" --commitMessage "%APPVEYOR_REPO_COMMIT_MESSAGE%" --commitId "%APPVEYOR_REPO_COMMIT%"
