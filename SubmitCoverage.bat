9d0cfd5e6c1112058656c49cf87068c80beb4438
Donovan Solms <donovan.solms@gmail.com>
2016-08-24 22:28:57 +0200

    Check coverage xml file

--- SubmitCoverage.bat
+++ SubmitCoverage.bat
@@ -2,4 +2,6 @@
 echo NuGet installing coveralls.net package
 nuget install coveralls.net -Version 0.7.0 -OutputDirectory tools
 echo Submitting coverage report to coveralls.io
+dir .\TestResults
+type .\TestResults\Coverage.xml
 .\tools\coveralls.net.0.7.0\tools\csmacnz.Coveralls.exe --opencover -i .\TestResults\Coverage.xml --commitAuthor "%APPVEYOR_REPO_COMMIT_AUTHOR%" --commitMessage "%APPVEYOR_REPO_COMMIT_MESSAGE%"
