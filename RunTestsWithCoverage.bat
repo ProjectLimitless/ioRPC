8608916c87edf38a9178d2f0b85a9ce110d91857
Donovan Solms <donovan.solms@gmail.com>
2016-08-24 22:05:18 +0200

    Updated path for coverage output

--- RunTestsWithCoverage.bat
+++ RunTestsWithCoverage.bat
@@ -1,3 +1,3 @@
 @echo on
 echo Running NUnit tests with OpenCover coverage
-OpenCover.Console.exe -target:"nunit3-console.exe"  -targetargs:"ioRPC.Test\bin\Debug\ioRPC.Test.dll --result=TestResults\Unit.xml" -filter:"+[*]* -[*.Test]*" -register:user -output:"TestResults\Coverage.xml" -showunvisited
+OpenCover.Console.exe -target:"nunit3-console.exe"  -targetargs:"ioRPC.Test\bin\Debug\ioRPC.Test.dll --result=.\TestResults\Unit.xml" -filter:"+[*]* -[*.Test]*" -register:user -output:".\TestResults\Coverage.xml" -showunvisited
