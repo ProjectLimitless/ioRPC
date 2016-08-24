a849c09edfa5934eea84a09101bb6b6484843893
Donovan Solms <donovan.solms@gmail.com>
2016-08-24 22:37:49 +0200

    Drop debug build

--- SubmitCoverage.bat
+++ SubmitCoverage.bat
@@ -4,5 +4,4 @@ nuget install coveralls.net -Version 0.7.0 -OutputDirectory tools
 echo Submitting coverage report to coveralls.io
 dir .\TestResults
 type .\TestResults\Coverage.xml
-echo "%APPVEYOR_JOB_NAME%"
 .\tools\coveralls.net.0.7.0\tools\csmacnz.Coveralls.exe --opencover -i .\TestResults\Coverage.xml --commitAuthor "%APPVEYOR_REPO_COMMIT_AUTHOR%" --commitMessage "%APPVEYOR_REPO_COMMIT_MESSAGE%"
